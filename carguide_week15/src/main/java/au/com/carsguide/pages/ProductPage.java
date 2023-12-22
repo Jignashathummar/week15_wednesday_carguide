b29a72b3f4c6dd94ac1a9c28026856540690a42b
Jignashathummar <jignashakotadiya10@gmail.com>
2023-12-22 11:14:51 +0000

    carguide

---
+++ carguide_week15/src/main/java/au/com/carsguide/pages/ProductPage.java
@@ -0,0 +1,38 @@
+package au.com.carsguide.pages;
+
+import au.com.carsguide.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(ProductPage.class.getName());
+
+    @CacheLookup
+    @FindBy (xpath = "//h1[@class = 'h1Text search--title']")
+    WebElement productPageTitle;
+    @CacheLookup
+    @FindBy(xpath = "//h1[@class='noResultsContent--h1Text']")
+    WebElement errorMsg;
+    public String getTitleOfThePage() {
+        log.info("Getting title of the page and verifying the make name : " + productPageTitle.toString());
+        return getTextFromElement(productPageTitle);
+    }
+    public String getErrorMessage() {
+        log.info("Getting error message when criteria was not match: " + productPageTitle.toString());
+        return getTextFromElement(errorMsg);
+    }
+
+    public boolean isTitleAvailable() {
+        try {
+            productPageTitle.getText();
+            return true;
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+}
---
+++ carguide_week15/src/test/java/au/com/carsguide/runner/RunCukeTest.java
@@ -0,0 +1,13 @@
+package au.com.carsguide.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "au/com/carsguide/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+}
